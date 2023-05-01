9b1c4ec1ca4bf16f65a7bc76b5840f58d1ca6c5e
Nadir Hossain <nadirhossain336@gmail.com>
2023-05-01 19:21:48 +0600

    implement chat page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import Chat from "./components/Chat"
 import Navbar from "./components/Navar"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Routes>
+        <Route path="/chat" element={<Chat/>}></Route>
+      </Routes>
     </>
   )
 }
